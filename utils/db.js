c0d012820363203705c9c9510007df38e81ec316
S.K.Siva <88235149+S-K-Siva@users.noreply.github.com>
2024-06-13 11:41:54 +0530

    changed mongodb local to global

--- utils/db.js
+++ utils/db.js
@@ -1,6 +1,7 @@
 import mongoose from "mongoose";
 try{
-    mongoose.connect('mongodb://localhost:27017/eccomerce');
+    // mongoose.connect('mongodb://localhost:27017/eccomerce');
+    mongoose.connect('mongodb+srv://admin:admin@mycluster.cqkoeiz.mongodb.net/')
     console.log("MongoDB is connected successfully!");
 }
 catch(err){
